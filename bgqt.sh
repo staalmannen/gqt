2dd083827ebd9cfa38d6fc8435f90b5854a58666
Robin Burchell <w00t@inspircd.org>
2009-07-17 23:48:28 +0100

    Allow build scripts to build different apps depending on command line input.

--- bgqt.sh
+++ bgqt.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-g++ examples/test.c -o test -I/home/w00t/gqt -I/home/w00t/qt4-bin/include -I/home/w00t/qt4-bin/include/QtGui -I/home/w00t/qt4-bin/include/QtCore -L/home/w00t/qt4-bin/lib -lQtCore -lQtGui -Wl,--rpath -Wl,/home/w00t/qt4-bin/lib
-./test
+if [ ! -n "$1" ]
+then
+	test="test"
+	echo "no test specified, running test"
+else
+	echo "running $1"
+	test=$1
+fi
+
+g++ "examples/$test.c" -o "$test" -I/home/w00t/gqt -I/home/w00t/qt4-bin/include -I/home/w00t/qt4-bin/include/QtGui -I/home/w00t/qt4-bin/include/QtCore -L/home/w00t/qt4-bin/lib -lQtCore -lQtGui -Wl,--rpath -Wl,/home/w00t/qt4-bin/lib
+./$test
--- bgtk.sh
+++ bgtk.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
-gcc examples/test.c -o test `pkg-config --cflags --libs gtk+`
-./test
+if [ ! -n "$1" ]
+then
+	test="test"
+	echo "no test specified, running test"
+else
+	echo "running $1"
+	test=$1
+fi
+
+gcc "examples/$test.c" -o "$test" `pkg-config --cflags --libs gtk+`
+./$test
