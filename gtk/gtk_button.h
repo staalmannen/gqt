d2d53e391cb3bcb31c80948753d548b3f8e77a9b
Robin Burchell <w00t@inspircd.org>
2009-07-18 17:52:26 +0100

    Add gtk_toggle_button_new_with_label()

--- gtk/gtk_button.h
+++ gtk/gtk_button.h
@@ -6,3 +6,11 @@ GtkWidget *gtk_button_new_with_label(const gchar *text)
 	return new GtkButton(text, NULL);
 }
 
+GtkWidget *gtk_toggle_button_new_with_label(const gchar *text)
+{
+	Q_ASSERT(text);
+
+	GtkButton *b = new GtkButton(text, NULL);
+	b->setCheckable(true);
+	return b;
+}
