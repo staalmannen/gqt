6cc15c7b2f6ccdf9e29cd7c751375020b9da3f38
Robin Burchell <w00t@inspircd.org>
2009-07-18 15:32:31 +0100

    Remove a bunch of ugly hacks that we were forced to use for GtkEntry previously.

--- gtk/gtk_entry.h
+++ gtk/gtk_entry.h
@@ -5,31 +5,25 @@ GtkWidget *gtk_entry_new()
 }
 
 // XXX: is this deprecated?
-// XXX: dynamic_cast throws? or does it leave e NULL if the cast fails.. we want to Q_ASSERT this if it is the latter!
 void gtk_entry_set_editable(GtkEntry *entry, gboolean editable)
 {
 	Q_ASSERT(entry);
 
-	GtkEntry *e = dynamic_cast<GtkEntry *>(entry);
-	e->setReadOnly(editable);
+	entry->setReadOnly(editable);
 }
 
-// XXX: dynamic_cast throws? or does it leave e NULL if the cast fails.. we want to Q_ASSERT this if it is the latter!
 void gtk_entry_set_text(GtkEntry *entry, const gchar *text)
 {
 	Q_ASSERT(entry);
 
-	GtkEntry *e = dynamic_cast<GtkEntry *>(entry);
-	e->setText(text);
+	entry->setText(text);
 }
 
 void gtk_entry_append_text(GtkEntry *entry, const gchar *text)
 {
 	Q_ASSERT(entry);
 
-	GtkEntry *e = dynamic_cast<GtkEntry *>(entry);
-
 	// Yuck, QLineEdit has no append() method, unlike QTextEdit
-	e->setText(e->text() + text);
+	entry->setText(entry->text() + text);
 }
 
