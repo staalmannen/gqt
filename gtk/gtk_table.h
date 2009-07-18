66e6bb09c7618e7cca20931f3249b3c8ea0d5bed
Robin Burchell <w00t@inspircd.org>
2009-07-18 15:36:55 +0100

    Remove unneeded documentation.

--- gtk/gtk_table.h
+++ gtk/gtk_table.h
@@ -19,20 +19,6 @@ void gtk_table_attach(GtkTable *table, GtkWidget *child,
 		int xopt, int yopt,
 		guint xpadding, guint ypadding)
 {
-	/*
-	 * Adds a widget to a table. The number of 'cells' that a widget will occupy is specified by left_attach, right_attach, top_attach and bottom_attach.
-	 * These each represent the leftmost, rightmost, uppermost and lowest column and row numbers of the table. (Columns and rows are indexed from zero). 
-	 * table: The GtkTable to add a new widget to. 
-	 * child: The widget to add. 
-	 * left_attach: the column number to attach the left side of a child widget to. 
-	 * right_attach: the column number to attach the right side of a child widget to. 
-	 * top_attach: the row number to attach the top of a child widget to. 
-	 * bottom_attach: the row number to attach the bottom of a child widget to. 
-	 * xoptions: Used to specify the properties of the child widget when the table is resized. 
-	 * yoptions: The same as xoptions, except this field determines behaviour of vertical resizing. 
-	 * xpadding: An integer value specifying the padding on the left and right of the widget being added to the table. 
-	 * ypadding: The amount of padding above and below the child widget.
-	 */
 	Q_ASSERT(table && table->layout() && child);
 
 	// Make sure the column/rowspan values are going to be kosher
