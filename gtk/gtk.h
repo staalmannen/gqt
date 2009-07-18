c7466108dd6a9c9de6a05c9eee51dd7b2ce07047
Robin Burchell <w00t@inspircd.org>
2009-07-18 01:43:39 +0100

    Minor GTK compatibility tweak: set alignment on newly created labels to center.

--- gtk/gtk.h
+++ gtk/gtk.h
@@ -118,8 +118,12 @@ void gtk_container_border_width(GtkContainer *container, guint border_width)
 GtkWidget *gtk_label_new(const gchar *text)
 {
 	Q_ASSERT(text);
+	GtkLabel *g = new GtkLabel(text, NULL);
 
-	return new GtkLabel(text, NULL);
+	// Set alignment so we mimic GTK's default.
+	g->setAlignment(Qt::AlignCenter);
+
+	return g;
 }
 
 GtkWidget *gtk_button_new_with_label(const gchar *text)
