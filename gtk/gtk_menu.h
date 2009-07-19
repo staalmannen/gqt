48900dfac18530cc8c455c9890b7286fd0ed3063
Robin Burchell <w00t@inspircd.org>
2009-07-19 18:35:10 +0100

    Fix gqt_current_mb hack, it seems that QWidget::setParent shouldn't be used on a QMenu. The QObject version works for what we need, though. Thanks to Thorbjørn.

--- gtk/gtk_menu.h
+++ gtk/gtk_menu.h
@@ -1,82 +1,3 @@
-#if 0
-
-
-  /*-- Create the menu bar --*/
-  menubar = gtk_menu_bar_new();
-
-  /*-- Add the menubar to the vbox --*/
-  gtk_box_pack_start(GTK_BOX(vbox), menubar, FALSE, TRUE, 0);
-  gtk_widget_show(menubar);
-
-  /*-- Add the text area to the window --*/
-  gtk_container_add(GTK_CONTAINER(vbox), text);  
-
-  /*-- Add the vbox to the main window --*/
-  gtk_container_add(GTK_CONTAINER(window), vbox);
-  
-  /*---------------- Create File menu items ------------------*/
-
-  // Create top menu item
-  menuFile = gtk_menu_item_new_with_label ("File");
-
-  // Append to menu bar
-  gtk_menu_bar_append (GTK_MENU_BAR(menubar), menuFile);
-  gtk_widget_show(menuFile);
-
-  /*-- Create File submenu  --*/
-  menu = gtk_menu_new();
-  gtk_menu_item_set_submenu(GTK_MENU_ITEM(menuFile), menu);
-
-  /*-- Create New menu item under File submenu --*/
-  menuitem = gtk_menu_item_new_with_label ("New");
-  gtk_menu_append(GTK_MENU(menu), menuitem);
-  gtk_widget_show (menuitem);
-
-#endif
-
-/*
- * XXX: this is to work around a (possible?) qt bug or odd behaviour that I can't quite put a handle on yet.
- * if a QMenuBar is created with a NULL parent, and later setParent()'d, it will flat out behave weirdly.
- *
- * this totally goes away if it is initialised with a parent..
- * test case code (obviously, modify as needed):
- #if 0
-	gtk_init (&argc, &argv);
-	// Create window
-	QWidget *parent = new QWidget(NULL);
-	QHBoxLayout *hb = new QHBoxLayout(NULL);
-	hb->setParent(parent);
-	parent->setLayout(hb);
-
-	// Create menubar
-	QMenuBar *mb = new QMenuBar(NULL);
-	mb->setParent(parent);
-
-	// Top level menu
-	QMenu *toplevel = new QMenu("hi there", parent); <----- problem is here
-//	toplevel->setParent(mb); <---- problem is here
-	mb->addMenu(toplevel);
-
-	QMenu *subitem = new QMenu("oh hi", NULL);
-	subitem->setParent(toplevel);
-	toplevel->addMenu(subitem);
-
-	subitem = new QMenu("fzt", NULL);
-	subitem->setParent(toplevel);
-	toplevel->addMenu(subitem);
-
-	// Pack menubar
-	parent->layout()->setMenuBar(mb);
-	parent->layout()->addWidget(new QTextEdit(parent));
-
-
-	parent->show();
-	gtk_main();
-	exit(0);
-#endif
- *
- */
-static GtkMenuBar *gqt_current_mb = NULL;
 
 void gtk_menu_bar_append(GtkMenuBar *menubar, GtkWidget *widget)
 {
@@ -88,14 +9,13 @@ void gtk_menu_bar_append(GtkMenuBar *menubar, GtkWidget *widget)
 	Q_ASSERT(menubar && m);
 
 	menubar->addMenu(m);
-	m->setParent(menubar);
+	m->QObject::setParent(menubar);
 }
 
 // Create a menu bar
 GtkWidget *gtk_menu_bar_new()
 {
-	gqt_current_mb = new QMenuBar(NULL);
-	return gqt_current_mb;
+	return new QMenuBar(NULL);
 }
 
 // Create a submenu
@@ -115,17 +35,14 @@ void gtk_menu_item_set_submenu(GtkMenuItem *menu_item, GtkWidget *submenu)
 	gqt_menu_hack_lookup[submenu] = menu_item;
 
 	// but ensure the fake gtk_menu_new() item is deleted when the lookup menu is deleted.
-	submenu->setParent(menu_item);
+	submenu->QObject::setParent(menu_item);
 }
 
 GtkWidget *gtk_menu_item_new_with_label(const gchar *text)
 {
 	Q_ASSERT(text);
 
-	// See comment where gqt_current_mb is defined for why this is done
-	Q_ASSERT(gqt_current_mb);
-
-	QWidget *q = new QMenu(text, gqt_current_mb);
+	QWidget *q = new QMenu(text, NULL);
 	//qDebug("gtk_menu_item_new_with_label(): returning %p", q);
 	return q;
 }
@@ -146,6 +63,6 @@ void gtk_menu_append(GtkMenuShell *menu_shell, GtkWidget *child)
 	Q_ASSERT(m && child_item);
 
 	m->addMenu(child_item);
-	child_item->setParent(m);
+	child_item->QObject::setParent(m);
 }
 
