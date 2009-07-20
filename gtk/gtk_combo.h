1d3517d0f34b9f8798b335666a77400ea6a064bc
Robin Burchell <w00t@inspircd.org>
2009-07-20 19:48:23 +0100

    gtk_combo_box_new_text() and friends for text combo manipulation.

--- gtk/gtk_combo.h
+++ gtk/gtk_combo.h
@@ -1,3 +1,40 @@
+/* New combo box implementation. Apparantly GtkCombo wasn't good enough ;p */
+typedef QComboBox GtkComboBox;
+#define GTK_COMBO_BOX(x) dynamic_cast<QComboBox *>(x)
+
+GtkWidget *gtk_combo_box_new_text()
+{
+	return new GtkComboBox();
+}
+
+void gtk_combo_box_append_text(GtkComboBox *combo_box, const gchar *text)
+{
+	Q_ASSERT(combo_box && text);
+
+	combo_box->addItem(text);
+}
+
+void gtk_combo_box_insert_text(GtkComboBox *combo_box, gint position, const gchar *text)
+{
+	Q_ASSERT(combo_box && text);
+
+	combo_box->insertItem(position, text);
+}
+
+void gtk_combo_box_prepend_text(GtkComboBox *combo_box, const gchar *text)
+{
+	Q_ASSERT(combo_box && text);
+
+	combo_box->insertItem(0, text);
+}
+
+void gtk_combo_box_remove_text(GtkComboBox *combo_box, gint position)
+{
+	Q_ASSERT(combo_box);
+
+	combo_box->removeItem(position);
+}
+
 /**************************************************************
  * This code implements obsolete GTK interfaces.
  *************************************************************/
