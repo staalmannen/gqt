c295746ae3b9b3978e27be9c76e3035ff2af0a4d
Robin Burchell <w00t@inspircd.org>
2009-07-20 02:38:51 +0100

    Q_ASSERT usage.

--- gtk/gtk_about_dialog.h
+++ gtk/gtk_about_dialog.h
@@ -19,20 +19,23 @@ GtkWidget *gtk_about_dialog_new()
 
 void gtk_about_dialog_set_program_name(GtkAboutDialog *diag, const gchar *s)
 {
-
+	Q_ASSERT(diag && s);
 }
 
 void gtk_about_dialog_set_licence(GtkAboutDialog *diag, const gchar *s)
 {
+	Q_ASSERT(diag && s);
 
 }
 
 void gtk_about_dialog_set_version(GtkAboutDialog *diag, const gchar *s)
 {
+	Q_ASSERT(diag && s);
 
 }
 
 void gtk_about_dialog_set_comments(GtkAboutDialog *diag, const gchar *s)
 {
+	Q_ASSERT(diag && s);
 
 }
