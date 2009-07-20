6d77318dacc00bb821ba3132bdef298bea4216f3
Robin Burchell <w00t@inspircd.org>
2009-07-20 20:55:18 +0100

    gtk_label_new() can take a NULL text parameter. Account for this.

--- gtk/gtk_label.h
+++ gtk/gtk_label.h
@@ -1,8 +1,7 @@
 
 GtkWidget *gtk_label_new(const gchar *text)
 {
-	Q_ASSERT(text);
-	GtkLabel *g = new GtkLabel(text, NULL);
+	GtkLabel *g = new GtkLabel(text);
 
 	// Set alignment so we mimic GTK's default.
 	g->setAlignment(Qt::AlignCenter);
