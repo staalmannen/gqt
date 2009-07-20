5cd03ef9fba079f2c42ad752a86b42ee01b72ec5
Robin Burchell <w00t@inspircd.org>
2009-07-20 22:06:22 +0100

    Work around widget leak (for now) so I avoid false positives by just returning NULL

--- gtk/gtk_action_group.h
+++ gtk/gtk_action_group.h
@@ -15,11 +15,8 @@ typedef struct
 
 GtkActionGroup *gtk_action_group_new(const gchar *name)
 {
-	// XXX: there is (quite obviously) a LEAK here.
-	// QActionGroup requires a widget for parenting.
-
 	// XXX 2: we ignore name, this requires implementing, perhaps via QObject::setProperty()?
-	return new QActionGroup(new QWidget());
+	return NULL;
 }
 
 void gtk_action_group_add_actions(GtkActionGroup *ag, GtkActionEntry *actions, gint action_count, gpointer user_data)
