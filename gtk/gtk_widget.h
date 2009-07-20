86d06c55b6f405aaa52bdcd6f2cf5b642675b577
Robin Burchell <w00t@inspircd.org>
2009-07-20 19:59:50 +0100

    Implement gtk_widget_set_tooltip_text().

--- gtk/gtk_widget.h
+++ gtk/gtk_widget.h
@@ -13,3 +13,9 @@ void gtk_widget_destroy(GtkWidget *widget)
 	delete widget;
 }
 
+void gtk_widget_set_tooltip_text(GtkWidget *widget, const gchar *text)
+{
+	Q_ASSERT(widget && text);
+
+	widget->setToolTip(text);
+}
