723ec0c06463248fde410fa87453d2356ab4417f
Robin Burchell <w00t@inspircd.org>
2009-07-21 12:47:57 +0100

    Add QtUiTools to build libraries to enable using QUiLoader (we will need this for gtk_ui_manager stuff).

--- bgqt.sh
+++ bgqt.sh
@@ -12,7 +12,7 @@ fi
 ~/qt4-bin/bin/moc -i gtk/gtk_button.h -o gtk/moc_gtk_button.h
 ~/qt4-bin/bin/moc -i gtk/gtk_entry.h -o gtk/moc_gtk_entry.h
 ~/qt4-bin/bin/moc -i gtk/gtk_combo.h -o gtk/moc_gtk_combo.h
-g++ "examples/$test.c" -o test -ggdb3 -I/home/w00t/gqt -I/home/w00t/qt4-bin/include -I/home/w00t/qt4-bin/include/QtGui -I/home/w00t/qt4-bin/include/QtCore -L/home/w00t/qt4-bin/lib -lQtCore -lQtGui -Wl,--rpath -Wl,/home/w00t/qt4-bin/lib `pkg-config --cflags --libs glib-2.0`
+g++ "examples/$test.c" -o test -ggdb3 -I/home/w00t/gqt -I/home/w00t/qt4-bin/include -I/home/w00t/qt4-bin/include/QtGui -I/home/w00t/qt4-bin/include/QtUiTools -I/home/w00t/qt4-bin/include/QtCore -L/home/w00t/qt4-bin/lib -lQtCore -lQtGui -l QtUiTools -Wl,--rpath -Wl,/home/w00t/qt4-bin/lib `pkg-config --cflags --libs glib-2.0`
 rm gtk/moc_gtk_button.h
 rm gtk/moc_gtk_entry.h
 rm gtk/moc_gtk_combo.h
