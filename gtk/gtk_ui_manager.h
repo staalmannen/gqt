5b071d0a18f1d493b57754ac72fc10af1807a09e
Robin Burchell <w00t@inspircd.org>
2009-07-21 12:48:24 +0100

    Start working on gtk_ui_manager UI generation from XML.

--- gtk/gtk_ui_manager.h
+++ gtk/gtk_ui_manager.h
@@ -1,10 +1,41 @@
-class GQTUIManager;
-typedef GQTUIManager GtkUIManager;
+#include <QXmlStreamReader>
 
+// Class to handle XML input and turn it into a tree of widgets.
+class GQTUIParser : public QXmlStreamReader
+{
+ public:
+	GQTUIParser(QString s) : QXmlStreamReader(s)
+	{
+	}
+
+	GtkWidget *parseUi()
+	{
+		while (!this->atEnd())
+		{
+			this->readNext();
+
+			if (this->isStartElement())
+			{
+				qDebug("GQTUIParser: Got %s with name %s", qPrintable(this->name().toString()), qPrintable(this->attributes().value("name").toString()));
+			}
+
+			if (this->isEndElement())
+			{
+			}
+		}
+
+		return NULL;
+	}
+};
+
+// Class to manage building UI from XML.
 class GQTUIManager
 {
-	// XXX: implement me
+ private:
+	GQTUIParser *parser;
 };
+typedef GQTUIManager GtkUIManager;
+
 
 GtkUIManager *gtk_ui_manager_new()
 {
@@ -18,7 +49,26 @@ void gtk_ui_manager_insert_action_group(GtkUIManager *uiman, GtkActionGroup *act
 
 void gtk_ui_manager_add_ui_from_string(GtkUIManager *uiman, const gchar *buffer, gssize length, GError **error)
 {
+	Q_ASSERT(uiman && buffer && error);
 
+	// GTK allows -1 if it's NULL terminated
+	if (length == -1)
+		length = strlen(buffer);
+
+	Q_ASSERT(length > 0);
+
+	GQTUIParser xml(QString(QByteArray(buffer, length)));
+
+	xml.parseUi();
+/*
+	QApplication a(argc, argv);
+	QWidget *p = new QWidget(NULL);
+	QUiLoader l;
+	l.createWidget("QComboBox", p, 0);
+	p->show();
+	a.exec();
+	exit(0);
+*/
 }
 
 GtkWidget *gtk_ui_manager_get_widget(GtkUIManager *uiman, const gchar *widget_name)
